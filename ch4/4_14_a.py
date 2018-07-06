72b59d75055e80988e5d4913c0f94b878b7ea5ba
sarangkhim <sarang.khim@gmail.com>
2018-07-07 00:33:48 +0900

    ex

---
+++ ch4/4_14_a.py
@@ -0,0 +1,5 @@
+'''
+4-14. PEP 8
+https://python.org/dev/peps/pep-0008/에서 PEP 8 스타일 가이드 원문을 읽어보세요. 공식 번역은 없지만 http://kenial.tistory.com/902에 번역문이 있으니 이를 참고하세요.
+'''
+
---
+++ ch4/4_1_b.py
@@ -0,0 +1,18 @@
+'''
+4-1. 피자
+좋아하는 피자를 최소한 세 가지 생각한 다음 피자 종류를 리스트에 저장하고, for 루프를 써서 피자의 종류를 모두 출력하세요.
+- for 루프를 수정해서 단순히 피자 종류만 출력하지 말고 문장을 출력하세요. 예를 들어 "나는 페퍼로니 피자를 좋아합니다." 같은 단순한 문장을 한 줄씩 출력해야 합니다.
+- 프로그램 마지막, for 루프 다음에 행을 추가해 당신이 피자를 얼마나 좋아하는지 나타내는 문장을 만드세요. 출력 결과는 당신이 좋아하는 피자에 관한 세 줄 이상의 문장과 "나는 피자를 정말 좋아해요!" 같은 문장이어야 합니다.
+
+Output:
+pepperoni
+hawaiian
+veggie
+
+I really love pepperoni pizza!
+I really love hawaiian pizza!
+I really love veggie pizza!
+
+I really love pizza!
+'''
+
